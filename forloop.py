0be2e7985214bc0244e89b189880b84e3f7ce413
Muhammad Awais Naveed <awaisn872@gmail.com>
2026-07-03 16:12:33 +0500

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,17 @@
+# for loop(used for sequential traversals)
+"""
+
+list=['a','b','c']
+for el in list:
+  print(el)         
+  
+tupple=(1,2,3,4,5,6)
+for num in tupple:
+    print(num)  
+    
+string="Awais"
+for char in string:
+    print(char)    
+else:
+    print("ENd")
+    """
